8497cd7f424bebd51c53576fe6cf231cab8bef8b
title6142 <61021123@up.ac.th>
2021-10-25 23:08:42 +0700

    เพิ่มข้อมูล

--- lib/Data/book_data.dart
+++ lib/Data/book_data.dart
@@ -17,10 +17,52 @@ final allBooks = <Book>[
   ),
   Book(
     id: 3,
-    author: 'ระดับ 1',
-    title: 'MANNA',
+    author: 'ระดับ 3',
+    title: 'KKKKLLLL',
     urlImage:
-        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick250486.jpg?alt=media&token=9c900c36-76f1-479f-a602-f23ca4c2944e',
+        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick719074.jpg?alt=media&token=0778cf31-2e03-4e05-9b12-7091f4accbc0',
   ),
-  
+  Book(
+    id: 4,
+    author: 'ระดับ 2',
+    title: 'Qq',
+    urlImage:
+        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick340339.jpg?alt=media&token=c727ba2d-c400-4b34-81a3-849c452d4570',
+  ),
+  Book(
+    id: 5,
+    author: 'ระดับ 1',
+    title: 'Errrrrrrrrr',
+    urlImage:
+        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick82556.jpg?alt=media&token=f2b00aef-043d-4332-b31d-4574e0fd31c8',
+  ),
+  Book(
+    id: 6,
+    author: 'ระดับ 1',
+    title: 'aaaasss',
+    urlImage:
+        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick532854.jpg?alt=media&token=30713b91-891c-4066-8455-51cf019226d0',
+  ),
+  Book(
+    id: 7,
+    author: 'ระดับ 2',
+    title: 'EEEEEE',
+    urlImage:
+        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick528437.jpg?alt=media&token=5d6257b2-01e3-434b-ae78-d88e99fe0a16',
+  ),
+  Book(
+    id: 8,
+    author: 'ระดับ 3',
+    title: 'gggggggggg',
+    urlImage:
+        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick959329.jpg?alt=media&token=5f057e2c-f22a-4009-a48d-7b4ef0a474b4',
+  ),
+  Book(
+    id: 9,
+    author: 'ระดับ 1',
+    title: 'POOO',
+    urlImage:
+        'https://firebasestorage.googleapis.com/v0/b/bedridden-cab42.appspot.com/o/sick%2Fsick323071.jpg?alt=media&token=1d85c639-a06b-4d38-864c-38b39dbbb974',
+  ),
+
 ];
